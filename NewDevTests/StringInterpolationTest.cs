9006a8c8def71a29bee8c3d2be9d971293b1648f
Lukáš Ostrý <lukas.ostry@agentes-it.com>
2018-12-22 20:17:56 +0100

    #NOREF - Added string interpolation.

--- NewDevTests/StringInterpolationTest.cs
+++ NewDevTests/StringInterpolationTest.cs
@@ -17,11 +17,11 @@ namespace NewDevTests
             var lastName = "Prijmeni";
             var birthDate = new DateTime(1995, 5, 6);
             var today = new DateTime(2019, 1, 1);
-            var age = 0;
-            var greeting = $"";
+            var age = ((today - birthDate).Days * 100 / 36525);
+            var greeting = $"Ahoj {firstName} {lastName}, narozeny {birthDate.ToString("d. M. yyyy")}, dnes ti je {age}";
                                  
 
-            Assert.AreEqual("Ahoj Jmeno Prijmeni, narozeny 6. 5. 1995, dnes ti je ", greeting);
+            Assert.AreEqual("Ahoj Jmeno Prijmeni, narozeny 6. 5. 1995, dnes ti je 23", greeting);
         }
     }
 }
