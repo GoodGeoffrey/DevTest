a93511d2b6ccfd4f4bda5e0cb61ac1e87a1157ac
Lukáš Ostrý <lukas.ostry@agentes-it.com>
2019-01-15 10:54:11 +0100

    #NOREF - Prepared sql insert query.

--- NewDevTests/SQLGenTest.cs
+++ NewDevTests/SQLGenTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace NewDevTests
@@ -22,11 +23,25 @@ namespace NewDevTests
                     CreationDate = DateTime.Today.AddDays(-a)
                 }).ToList();
 
+            // VALUES have 1000 limit, so if the sourceData is bigger, it can be used something like this:
+            //var cycleCount = (sourceData.Count - 1) / 1000 + 1;
+            //for (int i = 0; i < cycleCount; i++)
+            //{ }
 
-
-
+            string insertQuery =
+                $"INSERT INTO imaginary_ToSaveToDatabase " +
+                $"({nameof(ToSaveToDatabase.Id)}, {nameof(ToSaveToDatabase.Name)}, {nameof(ToSaveToDatabase.Code)}, {nameof(ToSaveToDatabase.CreationDate)}, {nameof(ToSaveToDatabase.LastChange)}) " +
+                $"VALUES " +
+                $"{GetFormatedStringToSaveToDatabase(sourceData)}";
 
             Assert.Inconclusive();
+        }
+
+        private string GetFormatedStringToSaveToDatabase(List<ToSaveToDatabase> sourceData)
+        {
+            StringBuilder sb = new StringBuilder();
+            sourceData.ForEach(item => sb.AppendLine($"('{item.Id}', '{item.Name}', '{item.Code}', '{item.CreationDate.ToString("yyyyMMdd HH:mm:ss")}', NULL)"));
+            return sb.ToString();
         }
 
         class ToSaveToDatabase
